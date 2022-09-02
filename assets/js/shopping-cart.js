c09bb98a7bba9f034557509c0973fac460c973b4
azharanowar <azharanowar@gmail.com>
2022-09-02 11:49:18 +0600

    Product one price update with JS

--- assets/js/shopping-cart.js
+++ assets/js/shopping-cart.js
@@ -1,20 +1,31 @@
 document.getElementById("productOneQuantityIncrease").addEventListener('click', function() {
-    productOneQuantity( true );
+    const productOneCurrentQuantity = productOneQuantity( true );
+    productOnePriceUpdate( 1250, productOneCurrentQuantity );
 });
 
 document.getElementById("productOneQuantityDecrease").addEventListener('click', function() {
-    productOneQuantity(false);
+    const productOneCurrentQuantity = productOneQuantity(false);
+    productOnePriceUpdate( 1250, productOneCurrentQuantity );
 });
 
 
 function productOneQuantity( isIncrease ) {
-    let productOneQuantityField = document.getElementById("productOneQuantity");
-    let productOneQuantityNumber = parseInt( document.getElementById("productOneQuantity").value );
+    const productOneQuantityField = document.getElementById("productOneQuantity");
+    const productOnePreviusQuantity = parseInt( document.getElementById("productOneQuantity").value );
+    let productOneCurrentQuantity = productOnePreviusQuantity;
     if ( isIncrease ) {
-        productOneQuantityField.value = productOneQuantityNumber + 1;
+        productOneCurrentQuantity = productOnePreviusQuantity + 1;
     } else {
-        if ( productOneQuantityNumber > 0 ) {
-            productOneQuantityField.value = productOneQuantityNumber - 1;
+        if ( productOnePreviusQuantity > 0 ) {
+            productOneCurrentQuantity = productOnePreviusQuantity - 1;
         }
     }
+
+    return productOneQuantityField.value = productOneCurrentQuantity;
+}
+
+
+function productOnePriceUpdate(price, quantity ) {
+    const totalPrice = price * quantity;
+    document.getElementById("productOnePrice").innerText = totalPrice;
 }
