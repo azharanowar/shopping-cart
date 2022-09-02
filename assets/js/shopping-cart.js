c014faa376a49bb198d21d7cd2cac8451d93e7fc
azharanowar <azharanowar@gmail.com>
2022-09-02 12:17:34 +0600

    Product two quantity and price update by JS

--- assets/js/shopping-cart.js
+++ assets/js/shopping-cart.js
@@ -18,26 +18,3 @@ document.getElementById("productTwoQuantityDecrease").addEventListener('click', 
     const productTwoCurrentQuantity = productTwoQuantity(false);
     productTwoPriceUpdate( 50, productTwoCurrentQuantity );
 });
-
-
-function productTwoQuantity( isIncrease ) {
-    const productTwoQuantityField = document.getElementById("productTwoQuantity");
-    const productTwoPreviusQuantity = parseInt( productTwoQuantityField.value );
-    let productTwoCurrentQuantity = productTwoPreviusQuantity;
-
-    if ( isIncrease ) {
-        productTwoCurrentQuantity = productTwoPreviusQuantity + 1;
-    } else {
-        if ( productTwoPreviusQuantity > 0 ) {
-            productTwoCurrentQuantity = productTwoPreviusQuantity - 1;
-        }
-    }
-
-    return productTwoQuantityField.value = productTwoCurrentQuantity; 
-}
-
-
-function productTwoPriceUpdate( price, quantity ) {
-    const totalPrice = price * quantity;
-    document.getElementById("productTwoPrice").innerText = totalPrice;
-}
