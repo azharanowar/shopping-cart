0c5702fd91170cbb7e27d06b0d799d238f36708e
azharanowar <azharanowar@gmail.com>
2022-09-02 12:16:47 +0600

    Product two quantity and price update with JS from  utilities.js file

--- assets/js/utilities.js
+++ assets/js/utilities.js
@@ -18,3 +18,26 @@ function productOnePriceUpdate(price, quantity ) {
     const totalPrice = price * quantity;
     document.getElementById("productOnePrice").innerText = totalPrice;
 }
+
+
+function productTwoQuantity( isIncrease ) {
+    const productTwoQuantityField = document.getElementById("productTwoQuantity");
+    const productTwoPreviusQuantity = parseInt( productTwoQuantityField.value );
+    let productTwoCurrentQuantity = productTwoPreviusQuantity;
+
+    if ( isIncrease ) {
+        productTwoCurrentQuantity = productTwoPreviusQuantity + 1;
+    } else {
+        if ( productTwoPreviusQuantity > 0 ) {
+            productTwoCurrentQuantity = productTwoPreviusQuantity - 1;
+        }
+    }
+
+    return productTwoQuantityField.value = productTwoCurrentQuantity; 
+}
+
+
+function productTwoPriceUpdate( price, quantity ) {
+    const totalPrice = price * quantity;
+    document.getElementById("productTwoPrice").innerText = totalPrice;
+}
